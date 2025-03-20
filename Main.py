ee1be7c6895b8761d914fe91bbb25ecc4b4ae750
B-rrrrrr <rayahunter15@gmail.com>
2025-03-20 15:47:48 +0100

    working mask

--- Main.py
+++ Main.py
@@ -1,30 +1,37 @@
 import pygame
+from pygame import FULLSCREEN
 
 pygame.init()
 
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((826, 532))
-map = pygame.image.load("map.webp").convert()
-map = pygame.transform.scale_by(map, 0.5)
+map = pygame.image.load("map.webp").convert_alpha()
 cursor = pygame.image.load("cursor.png").convert_alpha()
+cursor = pygame.transform.scale_by(cursor, 0.25)
+cx = cursor.get_width()/2
+cy = cursor.get_height()/2
 font = pygame.font.Font(None, 100)
 running = True
 
-mask = pygame.mask.from_surface(map)
-mask_2 = pygame.mask.from_surface(cursor)
+mask = pygame.mask.from_surface(map, 0)
+mask.fill()
+mask_2 = pygame.mask.from_surface(cursor, 0)
+
 while running:
     screen.fill((255, 255, 255))
     mx, my = pygame.mouse.get_pos()
-
-    #screen.blit(map, (0,0))
-    screen.blit(cursor, (mx, my))
+    screen.blit(map, (0,0))
+    screen.blit(cursor, (mx - cx, my - cx))
     text = font.render("Le Map", True, (0, 0, 0))
     screen.blit(text, (300, 200))
 
-    overlap_mask = mask.overlap_mask(mask_2, (mx, my))
-    screen.blit(overlap_mask.to_surface(unsetcolor=(0,0,0,0), setcolor=(255,0,0,255)), (0,0))
+    overlap_mask = mask.overlap_mask(mask_2, (mx - cx, my - cy))
+    screen.blit(overlap_mask.to_surface(None,map, None), (0,0))
 
     for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                running = False
         if event.type == pygame.QUIT:
             running = False
     pygame.display.flip()
