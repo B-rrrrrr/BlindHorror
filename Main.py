cab893323e9a4d6bc09ca98ffe9f8c5b1c5b9938
B-rrrrrr <rayahunter15@gmail.com>
2025-03-20 07:35:01 +0100

    update

--- Main.py
+++ Main.py
@@ -5,6 +5,7 @@ pygame.init()
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((826, 532))
 map = pygame.image.load("map.webp").convert()
+map = pygame.transform.scale_by(map, 0.5)
 cursor = pygame.image.load("cursor.png").convert_alpha()
 font = pygame.font.Font(None, 100)
 running = True
@@ -13,14 +14,15 @@ mask = pygame.mask.from_surface(map)
 mask_2 = pygame.mask.from_surface(cursor)
 while running:
     screen.fill((255, 255, 255))
-    pygame.transform.scale_by(map, 0.1)
-    pygame.transform.scale_by(cursor, 0.5)
     mx, my = pygame.mouse.get_pos()
 
-    screen.blit(map, (0,0))
+    #screen.blit(map, (0,0))
     screen.blit(cursor, (mx, my))
     text = font.render("Le Map", True, (0, 0, 0))
     screen.blit(text, (300, 200))
+
+    overlap_mask = mask.overlap_mask(mask_2, (mx, my))
+    screen.blit(overlap_mask.to_surface(unsetcolor=(0,0,0,0), setcolor=(255,0,0,255)), (0,0))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
