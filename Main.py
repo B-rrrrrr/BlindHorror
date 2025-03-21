ac4374d7f5c25aac96b9e186bb8b439763843900
B-rrrrrr <rayahunter15@gmail.com>
2025-03-21 11:45:23 +0100

    movement

--- Main.py
+++ Main.py
@@ -17,6 +17,19 @@ mask = pygame.mask.from_surface(map, 0)
 mask.fill()
 mask_2 = pygame.mask.from_surface(cursor, 0)
 
+mapX=2
+mapY=2
+mapS=16
+map_layout = [
+    [0, 41,42,43,44],
+    [0, 31,32,33,34],
+    [0, 21,22,23,24],
+    [0, 11,12,13,14],
+    [0]
+]
+map_layout.reverse()
+
+
 while running:
     screen.fill((255, 255, 255))
     mx, my = pygame.mouse.get_pos()
@@ -32,6 +45,16 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 running = False
+            if (event.key == pygame.K_w and mapY < 4):
+                mapY += 1
+            if event.key == pygame.K_s and mapY > 1:
+                mapY -= 1
+            if event.key == pygame.K_a and mapX > 1:
+                mapX -= 1
+            if event.key == pygame.K_d and mapX < 4:
+                mapX += 1
+            new_list = map_layout[mapY]
+            print(new_list[mapX])
         if event.type == pygame.QUIT:
             running = False
     pygame.display.flip()
